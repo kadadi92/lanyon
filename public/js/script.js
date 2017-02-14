d6af1714749e1394db6ce1636d9df8b0ed1c2ca1
kadadi92 <kadadi92@gmail.com>
2017-02-15 00:07:21 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $("#chinesenovelslink").click(function(){
         $("#chinesenovelslist").toggle("slow");
     });
+    $("#japanesenovelslink").click(function(){
+        $("#japanesenovelslist").toggle("slow");
+    });
 });
